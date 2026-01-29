dc93d7d60b394a6ba95aa9cc0d2ace0223525501
Xavier Fung <1157303+xavier114fch@users.noreply.github.com>
2026-01-29 11:22:34 +0000

    Only filter duplicates when there are more than 1 services under the slug

--- tnds_filter.py
+++ tnds_filter.py
@@ -107,31 +107,32 @@ def getSlugs(_data_dir) -> dict:
 		_total = len(_services)
 		_to_be_removed = []
 
-		for _i in range(1, _total):
-			_previous_service = _services[_i - 1]
-			_previous_start_date = _previous_service.get('startDate')
-			_previous_start_date = datetime.fromisoformat(_previous_start_date).date() if _previous_start_date else None
-			_previous_end_date = _previous_service.get('endDate')
-			_previous_end_date = datetime.fromisoformat(_previous_end_date).date() if _previous_end_date else None
-			_previous_last_modified = _previous_service.get('lastModified')
-			_previous_last_modified = datetime.fromisoformat(_previous_last_modified).date() if _previous_last_modified else None
+		if _total > 1:
+			for _i in range(1, _total):
+				_previous_service = _services[_i - 1]
+				_previous_start_date = _previous_service.get('startDate')
+				_previous_start_date = datetime.fromisoformat(_previous_start_date).date() if _previous_start_date else None
+				_previous_end_date = _previous_service.get('endDate')
+				_previous_end_date = datetime.fromisoformat(_previous_end_date).date() if _previous_end_date else None
+				_previous_last_modified = _previous_service.get('lastModified')
+				_previous_last_modified = datetime.fromisoformat(_previous_last_modified).date() if _previous_last_modified else None
 
-			_current_service = _services[_i]
-			_current_start_date = _current_service.get('startDate')
-			_current_start_date = datetime.fromisoformat(_current_start_date).date() if _current_start_date else None
-			_current_end_date = _current_service.get('endDate')
-			_current_end_date = datetime.fromisoformat(_current_end_date).date() if _current_end_date else None
-			_current_last_modified = _current_service.get('lastModified')
-			_current_last_modified = datetime.fromisoformat(_current_last_modified).date() if _current_last_modified else None
+				_current_service = _services[_i]
+				_current_start_date = _current_service.get('startDate')
+				_current_start_date = datetime.fromisoformat(_current_start_date).date() if _current_start_date else None
+				_current_end_date = _current_service.get('endDate')
+				_current_end_date = datetime.fromisoformat(_current_end_date).date() if _current_end_date else None
+				_current_last_modified = _current_service.get('lastModified')
+				_current_last_modified = datetime.fromisoformat(_current_last_modified).date() if _current_last_modified else None
 
-			if all((_previous_start_date, _previous_end_date, _previous_last_modified, _current_start_date, _current_end_date, _current_last_modified)):
-				if _previous_start_date == _current_start_date and _previous_end_date == _current_end_date and  _previous_last_modified <= _current_last_modified:
-					_to_be_removed.append(_i - 1)
-					_duplicated = _duplicated + 1
+				if all((_previous_start_date, _previous_end_date, _previous_last_modified, _current_start_date, _current_end_date, _current_last_modified)):
+					if _previous_start_date == _current_start_date and _previous_end_date == _current_end_date and  _previous_last_modified <= _current_last_modified:
+						_to_be_removed.append(_i - 1)
+						_duplicated = _duplicated + 1
 
-				elif _current_start_date < _previous_end_date:
-					_to_be_removed.append(_i - 1)
-					_overlapped = _overlapped + 1
+					elif _current_start_date < _previous_end_date:
+						_to_be_removed.append(_i - 1)
+						_overlapped = _overlapped + 1
 
 		_services = [_service for _i, _service in enumerate(_services) if _i not in _to_be_removed]
 
